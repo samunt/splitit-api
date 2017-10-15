741acb5975474276488cc4b780a490f98bfc0fce
Impuls3iv <sammy.unterman@gmail.com>
2017-10-14 21:06:31 -0400

    json web token stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011003408) do
+ActiveRecord::Schema.define(version: 20171015005127) do
 
   create_table "billeds", force: :cascade do |t|
     t.string   "firstName"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20171011003408) do
     t.string   "created_by"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
